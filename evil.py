85930c478999d5dc902533c020cb85599df982be
Pietro Cottone <pietro.cottone@gmail.com>
2022-10-17 23:12:49 +0200

    Initial commit

---
+++ evil.py
@@ -0,0 +1,2 @@
+# coding: latin-1
+a='aaaaa';print('Goodbye Cruel World!') if """ÊµÜ¶Ã0™´«³Ò§œŸ¹ªÈu6~õ…¢~Îû´ÑnKaùÏiJ³Q°Ôñv·Ã6aâ<jâç¥4mŒ~Ñ‰öïDÛhğ¦MR†§î'kt{ÛÛOÃA™ö´ŠõO£_+g™ôôä¤6˜‚¹¦RûèŠuOæ""" == """ÊµÜ¶Ã0™´«³Ò§œŸ¹ªÈu6~õ…¢~Îû´ÑnKaùÏiJ³Q°Ôñv·Ã6aâ<jâç¥4mŒ~Ñ‰öïDÛhğ¦MR†§î'kt{ÛÛOÃA™ö´ŠõO£_+g™ôôä¤6˜‚¹¦RûèŠuOæ""" else print('Hello World!')
---
+++ good.py
@@ -0,0 +1,3 @@
+# coding: latin-1
+a='aaaaa';print('Goodbye Cruel World!') if """ÊµÜ¶Ã0™´«³Ò§œŸ9ªÈu6~õ…¢~Îû´ÑnKaùÏiJ³Ñ¯Ôñv·Ã6aâ<jâç¥4mŒ~Ñ‰öïDÛhğ¦MR†'î'kt{ÛÛOÃA™ö´ŠõO£_+g™ôõä¤6˜‚¹¦Rûè
+uOæ""" == """ÊµÜ¶Ã0™´«³Ò§œŸ¹ªÈu6~õ…¢~Îû´ÑnKaùÏiJ³Q°Ôñv·Ã6aâ<jâç¥4mŒ~Ñ‰öïDÛhğ¦MR†§î'kt{ÛÛOÃA™ö´ŠõO£_+g™ôôä¤6˜‚¹¦RûèŠuOæ""" else print('Hello World!')
